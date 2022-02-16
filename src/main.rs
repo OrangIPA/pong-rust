06588c1e28b79590ddc6c156ab8f7c0fce06cb53
Bahana Alana Khansa <alanabahana102@gmail.com>
2022-02-17 00:32:59 +0700

    Finished

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,31 @@ enum Player {
     PlayerOne, PlayerTwo
 }
 
-
 struct Ball {
     pos: (f64, f64),
     dir: f64,
-    is_alive: bool,
     speed: f64,
     kuadran: (MovementHorizontal, MovementVertical),
 }
 
 impl Ball {
+    fn new() -> Ball{
+        let randdir: i32 = rand::thread_rng().gen_range(0..3);
+        let randdir = match randdir{
+            0 => (MovementHorizontal::Left, MovementVertical::Up),
+            1 => (MovementHorizontal::Left, MovementVertical::Down),
+            2 => (MovementHorizontal::Right, MovementVertical::Up),
+            3 => (MovementHorizontal::Right, MovementVertical::Down),
+            _ => (MovementHorizontal::None, MovementVertical::None),
+        };
+        Ball {
+            pos: (250.0, 150.0),
+            dir: rand::thread_rng().gen_range(0.5..(0.5 * PI - 0.5)),
+            speed: 8.0,
+            kuadran: randdir,
+        }
+    }
+
     fn render(&mut self, gl: &mut GlGraphics, arg: &RenderArgs){
         use graphics;
         let WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
@@ -47,9 +62,17 @@ impl Ball {
         });
     }
 
-    fn update(&mut self){
+    fn update(&mut self, pad: &mut (Paddle, Paddle)){
         let dx = self.speed * self.dir.sin();
         let dy = self.speed * self.dir.cos();
+
+        if self.pos.1 < pad.0.pos + 5.0{
+            pad.0.up_slow();
+        } else if self.pos.1 > pad.0.pos - 5.0{
+            pad.0.down_slow();
+        }
+
+        // Top Bounce
         if self.kuadran.1 == MovementVertical::Up {
             if dy < self.top_margin(){
                 self.pos.1 -= dy;
@@ -57,6 +80,7 @@ impl Ball {
                 self.pos.1 += self.top_margin() - dy;
                 self.kuadran.1 = MovementVertical::Down;
             }
+        // Bottom Bounce
         }else if self.kuadran.1 == MovementVertical::Down {
             if dy < self.bottom_margin(){
                 self.pos.1 += dy;
@@ -66,22 +90,93 @@ impl Ball {
             }
         }
 
+        // Right Bounce
         if self.kuadran.0 == MovementHorizontal::Right{
+
+            // Check if the Ball can get hit by the Paddle
             if dx < self.right_margin(){
+
+                // If the Ball should not get hit by the Paddle yet, update the position without bouncing
                 self.pos.0 += dx;
             }else {
-                self.pos.0 -= self.right_margin() - dx;
-                self.kuadran.0 = MovementHorizontal::Left;
+
+                // Check if the Ball hit the Paddle
+                if (self.pos.1 > pad.1.pos - 5.0) && (self.pos.1 < (pad.1.pos + pad.1.width + 5.0)){
+
+                    // If the Ball hit the Paddle, bounce it back
+                    self.pos.0 -= self.right_margin() - dx;
+                    
+                    // Change the Ball direction according to the Ball's location while hitting the Paddle
+                    self.dir = ((PI * (self.paddle_pos(&pad.1) - 30.0) / 90.0) - 0.5).abs().sin();
+                    self.kuadran.1 = if self.paddle_pos(&pad.1) > 0.0 {
+                        MovementVertical::Up
+                    } else {
+                        MovementVertical::Down
+                    };
+
+                    self.kuadran.0 = MovementHorizontal::Left;
+                }else{
+
+                    // If the Paddle miss the Ball, reset the Ball to the center
+                    self.pos = (250.0, 150.0);
+                    let randdir: i32 = rand::thread_rng().gen_range(0..3);
+                    let randdir = match randdir{
+                        0 => (MovementHorizontal::Left, MovementVertical::Up),
+                        1 => (MovementHorizontal::Left, MovementVertical::Down),
+                        2 => (MovementHorizontal::Right, MovementVertical::Up),
+                        3 => (MovementHorizontal::Right, MovementVertical::Down),
+                        _ => (MovementHorizontal::None, MovementVertical::None),
+                    };
+                    self.kuadran = randdir;
+                    self.dir =  rand::thread_rng().gen_range(0.5..(0.5 * PI - 0.5));
+                }
             }
+        // Left Bounce
         }else if self.kuadran.0 == MovementHorizontal::Left{
+
+            // Check if the Ball can get hit by the Paddle
             if dx < self.left_margin(){
+
+                // If the Ball should not get hit the Paddle yet, update the position without bouncing
                 self.pos.0 -= dx;
             }else {
-                self.pos.0 += self.left_margin() - dx;
-                self.kuadran.0 = MovementHorizontal::Right;
+
+                // Check if the Ball hit the Paddle
+                if (self.pos.1 > pad.0.pos - 5.0) && (self.pos.1 < (pad.0.pos + pad.0.width + 5.0)){
+
+                    // If the Ball hit the Paddle, bounce it back
+                    self.pos.0 += self.left_margin() - dx;
+
+                    // Change the Ball direction according to the Ball's location while hitting the Paddle
+                    self.dir = ((PI * (self.paddle_pos(&pad.0) - 30.0) / 90.0) - 0.5).abs().sin();
+                    self.kuadran.1 = if self.paddle_pos(&pad.0) > 0.0 {
+                        MovementVertical::Up
+                    } else {
+                        MovementVertical::Down
+                    };
+                    self.kuadran.0 = MovementHorizontal::Right;
+                }else{
+
+                    // If the Paddle miss the Ball, reset the Ball to the center
+                    self.pos = (250.0, 150.0);
+                    let randdir: i32 = rand::thread_rng().gen_range(0..3);
+                    let randdir = match randdir{
+                        0 => (MovementHorizontal::Left, MovementVertical::Up),
+                        1 => (MovementHorizontal::Left, MovementVertical::Down),
+                        2 => (MovementHorizontal::Right, MovementVertical::Up),
+                        3 => (MovementHorizontal::Right, MovementVertical::Down),
+                        _ => (MovementHorizontal::None, MovementVertical::None),
+                    };
+                    self.kuadran = randdir;
+                    self.dir =  rand::thread_rng().gen_range(0.5..(0.5 * PI - 0.5));
+                }
             }
         }
 
+    }
+
+    fn paddle_pos(&self, pad: &Paddle) -> f64{
+        -(self.pos.1 - (pad.pos + (pad.width / 2.0)))
     }
 
     fn top_margin(&self) -> f64{
@@ -93,11 +188,11 @@ impl Ball {
     }
 
     fn left_margin(&self) -> f64 {
-        self.pos.0 - 8.0
+        self.pos.0 - 16.0
     }
 
     fn right_margin(&self) -> f64 {
-        492.0 - self.pos.0
+        484.0 - self.pos.0
     }
 }
 
@@ -124,7 +219,7 @@ impl Game {
     fn update(&mut self){
         self.paddle.0.update();
         self.paddle.1.update();
-        self.ball.update();
+        self.ball.update(&mut self.paddle);
     }
 
     fn pressed(&mut self, btn: &Button){
@@ -141,8 +236,8 @@ impl Game {
 }
 
 struct Paddle {
-    pos: f32,
-    width: f32,
+    pos: f64,
+    width: f64,
     player: Player,
     movement: MovementVertical,
 }
@@ -190,6 +285,18 @@ impl Paddle {
             self.pos += 10.0;
         } 
     }
+
+    fn up_slow(&mut self){
+        if self.pos > 0.0{
+            self.pos -= 5.0;
+        }
+    }
+
+    fn down_slow(&mut self){
+        if self.pos < (300.0 - self.width){
+            self.pos += 5.0;
+        } 
+    }
 }
 
 fn main() {
@@ -202,38 +309,23 @@ fn main() {
     .build()
     .unwrap();
 
-    let randdir: i32 = rand::thread_rng().gen_range(0..3);
-    let randdir = match randdir{
-        0 => (MovementHorizontal::Left, MovementVertical::Up),
-        1 => (MovementHorizontal::Left, MovementVertical::Down),
-        2 => (MovementHorizontal::Right, MovementVertical::Up),
-        3 => (MovementHorizontal::Right, MovementVertical::Down),
-        _ => (MovementHorizontal::None, MovementVertical::None),
-    };
-
     let mut game = Game {
         gl: GlGraphics::new(opengl),
         paddle: (
             Paddle{
                 pos: 0.0,
-                width: 40.0,
+                width: 50.0,
                 player: Player::PlayerOne,
                 movement: MovementVertical::None,
             },
             Paddle{
                 pos: 0.0,
-                width: 40.0,
+                width: 50.0,
                 player: Player::PlayerTwo,
                 movement: MovementVertical::None,
             }
         ),
-        ball: Ball {
-            pos: (250.0, 150.0),
-            dir: rand::thread_rng().gen_range(0.0..(0.5 * PI)),
-            is_alive: true,
-            speed: 10.0,
-            kuadran: randdir,
-        }
+        ball: Ball::new(),
     };
     
     let mut events = Events::new(EventSettings::new()).ups(30);
